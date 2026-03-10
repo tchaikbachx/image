5b9d0d0f2aa8bb7d0ec361439c2a0678e25046d6
Adlumia7 <GrompBuh@gmail.com>
2026-03-10 05:18:51 -0500

    more python tinkering, made a function that adds an instrument to the instruments table in the db

--- app/app.py
+++ app/app.py
@@ -4,9 +4,24 @@ import sqlite3
 db = sqlite3.connect("database.db")
 cur = db.cursor()
 
+# initTables()
+# creates the tables in the db
+def initTables():
+    cur.execute("CREATE TABLE IF NOT EXISTS instrument(ID INT PRIMARY KEY, Name_ID, Old_ID, Type, Grade, Make, Model, Picture, Serial_Number, Price, Stored_In, Dept)")
+
 # addInstrument()
 # adds an instrument with given fields to the database
-def addInstrument():
-    cur.execute("CREATE TABLE instrument(ID, Name_ID, Old_ID, Type, Grade, Make, Model, Picture, Serial_Number, Price, Stored_In, Dept)")
+def addInstrument(ID, Name_ID, Old_ID, Type, Grade, Make, Model, Picture, Serial_Number, Price, Stored_In, Dept):
 
-addInstrument()
+    cur.execute("INSERT INTO instrument VALUES (" + ID + "," + Name_ID + "," + Old_ID + "," + Type + "," + Grade + "," + Make + "," + Model + "," + Picture + "," + Serial_Number + "," + Price + "," + Stored_In + "," + Dept + ")")
+
+# initialize tables
+initTables()
+
+addInstrument("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+
+# commit changes to db file
+db.commit()
+
+# close db connection
+db.close()
