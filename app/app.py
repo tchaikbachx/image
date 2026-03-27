ea1ceb81ccec146816359d01689fe3be88c0cd65
Collin Marshall <marshall@grinnell.edu>
2026-03-27 09:25:57 -0500

    edited the add functions to include types and eliminate the need to include quotes everywhere. Added a few extra items to the database. Note that a few things changed because of the typing (ie the checkoutable column is now true or false, not yes or no).

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 # connect to the database file
 db = sqlite3.connect("database.db")
@@ -18,45 +19,86 @@ def initTables():
 
 # addInstrument()
 # adds an instrument with given fields to the database
-def addInstrument(ID, Name_ID, Old_ID, Type, Grade, Make, Model, Picture, Serial_Number, Price, Stored_In, Dept):
-    cur.execute("INSERT INTO instrument VALUES (" + ID + "," + Name_ID + "," + Old_ID + "," + Type + "," + Grade + "," + Make + "," + Model + "," + Picture + "," + Serial_Number + "," + Price + "," + Stored_In + "," + Dept + ")")
+def addInstrument(ID: int, Name_ID: str, Old_ID: str, Type: str, Grade: str, Make: str, Model: str, Picture: str, Serial_Number: str, Price: float, Stored_In: int, Dept: int):
+    ID = str(ID)
+    Name_ID = str(Name_ID)
+    Old_ID = str(Old_ID)
+    Type = str(Type)
+    Grade = str(Grade)
+    Make = str(Make)
+    Model = str(Model)
+    Picture = str(Picture)
+    Serial_Number = str(Serial_Number)
+    Price = str(Price)
+    Stored_In = str(Stored_In)
+    Dept = str(Dept)
+    cur.execute("INSERT INTO instrument VALUES (" + ID + ",\'" + Name_ID + "\',\'" + Old_ID + "\',\'" + Type + "\',\'" + Grade + "\',\'" + Make + "\',\'" + Model + "\',\'" + Picture + "\',\'" + Serial_Number + "\'," + Price + "," + Stored_In + "," + Dept + ")")
 
 # addKkey()
 # adds a kkey with given fields to the database
 # the description field is for any additional information about the kkey, such as what it opens or where it is stored
-def addKkey(ID, Name_ID, Qty, Description):
-    cur.execute("INSERT INTO kkey VALUES (" + ID + "," + Name_ID + "," + Qty + "," + Description + ")")
+def addKkey(ID: int, Name_ID: str, Qty: int, Description: str):
+    ID = str(ID)
+    Name_ID = str(Name_ID)
+    Qty = str(Qty)
+    Description = str(Description)
+    cur.execute("INSERT INTO kkey VALUES (" + ID + ",\'" + Name_ID + "\'," + Qty + ",\'" + Description + "\')")
 
 # addLocker()
 # adds a locker with given fields to the database
 # Checkoutable is a boolean field that indicates whether the locker can be checked out or not
-def addLocker(ID, Name_ID, Combo, Kkey, Checkoutable):
-    cur.execute("INSERT INTO locker VALUES (" + ID + "," + Name_ID + "," + Combo + "," + Kkey + "," + Checkoutable + ")")
+def addLocker(ID: int, Name_ID: str, Combo: str, Kkey: int, Checkoutable: bool):
+    ID = str(ID)
+    Name_ID = str(Name_ID)
+    Combo = str(Combo)
+    Kkey = str(Kkey)
+    Checkoutable = str(Checkoutable)
+    cur.execute("INSERT INTO locker VALUES (" + ID + ",\'" + Name_ID + "\',\'" + Combo + "\'," + Kkey + ",\'" + Checkoutable + "\')")
 
 # addBorrower()
 # adds a borrower with given fields to the database
-def addBorrower(ID, Email):
-    cur.execute("INSERT INTO borrower VALUES (" + ID + "," + Email + ")")
+def addBorrower(ID: int, Email: str):
+    ID = str(ID)
+    Email = str(Email)
+    cur.execute("INSERT INTO borrower VALUES (" + ID + ",\'" + Email + "\')")
 
 # addCheckout()
 # adds a checkout record with given fields to the database
-def addCheckout(ID, Borrower_ID, Item_ID, Checkout_Date, Due_Date, Closed_Date):
-    cur.execute("INSERT INTO checkout VALUES (" + ID + "," + Borrower_ID + "," + Item_ID + "," + Checkout_Date + "," + Due_Date + "," + Closed_Date + ")")
+def addCheckout(ID: int, Borrower_ID: int, Item_ID: int, Checkout_Date: datetime.date, Due_Date: datetime.date, Closed_Date: datetime.date):
+    ID = str(ID)
+    Borrower_ID = str(Borrower_ID)
+    Item_ID = str(Item_ID)
+    Checkout_Date = str(Checkout_Date)
+    Due_Date = str(Due_Date)
+    Closed_Date = str(Closed_Date)
+    cur.execute("INSERT INTO checkout VALUES (" + ID + "," + Borrower_ID + "," + Item_ID + ",\'" + Checkout_Date + "\',\'" + Due_Date + "\',\'" + Closed_Date + "\')")
 
 # addMissing()
 # adds a missing item record with given fields to the database
-def addMissing(ID, Date_Missing, Date_Found, Item_ID, Description):
-    cur.execute("INSERT INTO missing VALUES (" + ID + "," + Date_Missing + "," + Date_Found + "," + Item_ID + "," + Description + ")")
+def addMissing(ID: int, Date_Missing: datetime.date, Date_Found: datetime.date, Item_ID: int, Description: str):
+    ID = str(ID)
+    Date_Missing = str(Date_Missing)
+    Date_Found = str(Date_Found)
+    Item_ID = str(Item_ID)
+    Description = str(Description)
+    cur.execute("INSERT INTO missing VALUES (" + ID + ",\'" + Date_Missing + "\',\'" + Date_Found + "\'," + Item_ID + ",\'" + Description + "\')")
 
 # addBroken()
 # adds a broken item record with given fields to the database
-def addBroken(ID, Date_Broken, Date_Fixed, Item_ID, Description):
-    cur.execute("INSERT INTO broken VALUES (" + ID + "," + Date_Broken + "," + Date_Fixed + "," + Item_ID + "," + Description + ")")
+def addBroken(ID: int, Date_Broken: datetime.date, Date_Fixed: datetime.date, Item_ID: int, Description: str):
+    ID = str(ID)
+    Date_Broken = str(Date_Broken)
+    Date_Fixed = str(Date_Fixed)
+    Item_ID = str(Item_ID)
+    Description = str(Description)
+    cur.execute("INSERT INTO broken VALUES (" + ID + ",\'" + Date_Broken + "\',\'" + Date_Fixed + "\'," + Item_ID + ",\'" + Description + "\')")
 
 # addDepartment()
 # adds a department with given fields to the database
-def addDepartment(ID, Department_Name):
-    cur.execute("INSERT INTO department VALUES (" + ID + "," + Department_Name + ")")
+def addDepartment(ID: int, Department_Name: str):
+    ID = str(ID)
+    Department_Name = str(Department_Name)
+    cur.execute("INSERT INTO department VALUES (" + ID + ",\'" + Department_Name + "\')")
 
 # toydb 
 def toydb():
@@ -115,16 +157,16 @@ initTables()
 
 
 #----- function calls to add entries to the database, for testing purposes:
-toydb()
-#addInstrument("2", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
-#addKkey("2", "'K25'", "3", "4")
-#addLocker("1", "2", "3", "4", "5")
-# addBorrower("1", "2")
-# addCheckout("1", "2", "3", "4", "5", "6")
-# addMissing("1", "2", "3", "4", "5")
-# addBroken("1", "2", "3", "4", "5")
-# addDepartment("1", "2")
-
+#toydb()
+# addDepartment(29, 'TDPS')
+# addBroken(30, '3/26/26', '0', 12, 'broken violin')
+# addBroken(31, '3/27/26', '4/1/26', 13, 'broken guitar')
+# addMissing(32, '3/26/26', '0', 13, 'missing guitar')
+# addCheckout(33, 19, 12, '3/28/26', '4/28/26', '0')
+# addBorrower(34, 'DoctorWho@whatishappening.org')
+# addLocker(35, 'A6', '0', 9, True)
+# addKkey(36, 'K18', 1, 'Opens auditorium storage closet')
+# addInstrument(37, 'Flute1', '789', 'Flute', 'A', 'Yamaha', 'model', 'image', '12345', 304.33, 35, 16)
 
 # commit changes to db file
 db.commit()
