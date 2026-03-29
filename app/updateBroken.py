1db6e7daa390cf76d5c55385f5eb3e8b2e859b1e
Adlumia7 <GrompBuh@gmail.com>
2026-03-29 18:07:22 -0500

    started functions for updating db entries

---
+++ app/updateBroken.py
@@ -0,0 +1,22 @@
+import sys
+import sqlite3
+
+import updateIfNotNull
+
+# connect to the database file
+db = sqlite3.connect("database.db")
+cur = db.cursor()
+
+# updateBroken()
+# updates a broken item record with given fields in the database
+def updateBroken(ID: int, Date_Broken: str, Date_Fixed: str, Item_ID: int, Description: str):
+    cur.execute("UPDATE broken SET " + updateIfNotNull.uINN("Date_Broken", Date_Broken) + ", " + updateIfNotNull.uINN("Date_Fixed", Date_Fixed) + ", " + updateIfNotNull.uINN("Item_ID", Item_ID) + ", " + updateIfNotNull.uINN("Description", Description) + " WHERE ID = " + str(ID))
+
+# call function with passed arguments
+updateBroken(int(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]), str(sys.argv[4]), str(sys.argv[5]))
+
+# commit changes to db file
+db.commit()
+
+# close db connection
+db.close()
---
+++ app/updateIfNotNull.py
@@ -0,0 +1,12 @@
+# updateIfNotNull(FieldName, change):
+# contains the string to update 'Field' to a new entry if the given `change`` is non-empty
+def updateIfNotNull(FieldName: str, change):
+    if change != None:
+        return (FieldName + " = " + str(change))
+    else:
+        return ""
+
+# uINN(FieldName, change):
+# alias for updateIfNotNull
+def uINN(FieldName: str, change):
+    return updateIfNotNull(FieldName, change)
